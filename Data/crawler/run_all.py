e178088218c58d32bd5170337c08878b69c46745
Harsh Jhamtani <harshjhamtani@gmail.com>
2019-06-27 09:17:27 -0700

    Update to download default page as well

--- Data/crawler/run_all.py
+++ Data/crawler/run_all.py
@@ -19,7 +19,7 @@ for i, link in enumerate(all_links):
 	if i>end: break
 	num = extra_links[i]
 	print "i,num: ",i,num # 0 is no extra, if extra then (num-1) extra pages
-	for j in range(1,num): # j=0 is the home page. Begin range from 0 if home page is to be included.
+	for j in range(0,num): # j=0 is the home page. Begin range from 0 if home page is to be included.
 		print j
 		processes.add( subprocess.Popen(["python", "save_rendered_webpage.py", "-i", str(i), "-num", str(j) ]) )
 		if len(processes) >= max_processes:
